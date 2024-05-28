51c76e0c33a39a9f965e28f3a20011c68ceb10a4
cumback <100005334+Justinzzz69@users.noreply.github.com>
2024-05-28 14:05:12 +0200

    Create update_uuid.py

---
+++ update_uuid.py
@@ -0,0 +1,16 @@
+import uuid
+import re
+
+def updateUUID(file_name):
+    with open(file_name, "r") as f:
+        content = f.read()
+
+    new_uuid_str = 'Global UUID := "' + uuid.uuid4().hex + '"'
+
+    content_new = re.sub('Global UUID := ".+"', new_uuid_str, content)
+
+    with open(file_name, "w") as f:
+        f.write(content_new)
+
+if __name__ == '__main__':
+    updateUUID('klex.ahk')
